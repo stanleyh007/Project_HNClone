b72d6386b340401c04689b0bb8458ccdfa6307aa
Christian Philip Ege Nielsen <kagejohn@kagejohn.dk>
2018-10-23 21:04:53 +0200

    Created story queries, missing rating
    Create a story (created)
    Get a story (created)
    Get 10 stories (created)
    Update a story (Rating) (missing)

--- Project_HNClone/Queries/StoryQueries.cs
+++ Project_HNClone/Queries/StoryQueries.cs
@@ -1,11 +1,248 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Project_HNClone.Data;
 
 namespace Project_HNClone.Queries
 {
     public class StoryQueries
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public StoryQueries(IConfiguration config)
+        {
+            this._configuration = config;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public bool CreateStory(Story story)
+        {
+            bool success = false;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                SqlTransaction transaction = connection.BeginTransaction();
+                command.Connection = connection;
+                command.Transaction = transaction;
+
+                try
+                {
+                    command.CommandText = "INSERT INTO Stories (Name, Content, CreatorID, CreatorName, PostType, PostURL) VALUES (@Name, @Content, @CreatorID, @CreatorName, @PostType, @PostURL)";
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar).Value = story.name;
+                    command.Parameters.Add("@Content", SqlDbType.NVarChar).Value = story.content;
+                    command.Parameters.Add("@CreatorID", SqlDbType.Int).Value = story.creatorID;
+                    command.Parameters.Add("@CreatorName", SqlDbType.NVarChar).Value = story.creatorName;
+                    command.Parameters.Add("@PostType", SqlDbType.NVarChar).Value = story.postType;
+                    command.Parameters.Add("@PostURL", SqlDbType.NVarChar).Value = story.postURL;
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    // Attempt to roll back the transaction.
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred
+                        // on the server that would cause the rollback to fail, such as
+                        // a closed connection.
+                    }
+                }
+            }
+
+            return success;
+        }
+
+        public Story GetStory(int id = 0, string postUrl = null)
+        {
+            Story story = new Story();
+
+            if (id != 0 || postUrl != null)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    SqlCommand command = new SqlCommand();
+                    SqlDataReader reader;
+                    command.Connection = connection;
+
+                    try
+                    {
+                        if (id != 0)
+                        {
+                            command.CommandText = "select * from Stories where Id = @Id";
+                            command.Parameters.AddWithValue("@Id", id);
+                        }
+
+                        else if (postUrl != null)
+                        {
+                            command.CommandText = "select * from Stories where PostURL = @PostURL";
+                            command.Parameters.AddWithValue("@PostURL", postUrl);
+                        }
+
+                        connection.Open();
+                        reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                if (reader.GetName(i) == "Id")
+                                {
+                                    story.id = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "Name")
+                                {
+                                    story.name = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "Content")
+                                {
+                                    story.content = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "CreatorID")
+                                {
+                                    story.creatorID = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "CreatorName")
+                                {
+                                    story.creatorName = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PostType")
+                                {
+                                    story.postType = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PostURL")
+                                {
+                                    story.postURL = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PositiveRating")
+                                {
+                                    story.positiveRating = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "NegativeRating")
+                                {
+                                    story.negativeRating = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PublishDate")
+                                {
+                                    story.publishDate = (DateTime)reader.GetValue(i);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                        Console.WriteLine("  Message: {0}", ex.Message);
+                    }
+                }
+            }
+
+            return story;
+        }
+
+        public List<Story> GetStories(int amount, string postType)
+        {
+            List<Story> stories = new List<Story>();
+
+            if (amount > 0)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    SqlCommand command = new SqlCommand();
+                    SqlDataReader reader;
+                    command.Connection = connection;
+
+                    try
+                    {
+                        if (postType == "new")
+                        {
+                            command.CommandText = "select top @amount * from Stories order by PublishDate desc";
+                            command.Parameters.AddWithValue("@amount", amount);
+                        }
+
+                        else
+                        {
+                            command.CommandText = "select top @amount * from Stories where PostType = @PostType order by PublishDate desc";
+                            command.Parameters.AddWithValue("@amount", amount);
+                            command.Parameters.AddWithValue("@PostType", postType);
+                        }
+
+                        connection.Open();
+                        reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            Story story = new Story();
+
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                if (reader.GetName(i) == "Id")
+                                {
+                                    story.id = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "Name")
+                                {
+                                    story.name = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "Content")
+                                {
+                                    story.content = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "CreatorID")
+                                {
+                                    story.creatorID = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "CreatorName")
+                                {
+                                    story.creatorName = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PostType")
+                                {
+                                    story.postType = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PostURL")
+                                {
+                                    story.postURL = (string)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PositiveRating")
+                                {
+                                    story.positiveRating = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "NegativeRating")
+                                {
+                                    story.negativeRating = (int)reader.GetValue(i);
+                                }
+                                if (reader.GetName(i) == "PublishDate")
+                                {
+                                    story.publishDate = (DateTime)reader.GetValue(i);
+                                }
+                            }
+
+                            stories.Add(story);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                        Console.WriteLine("  Message: {0}", ex.Message);
+                    }
+                }
+            }
+
+            return stories;
+        }
     }
 }
