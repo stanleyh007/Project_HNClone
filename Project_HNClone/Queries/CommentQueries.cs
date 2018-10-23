2ac37bd8ca7a38baaf8ea7ea11982f88b0000e94
TreasureSchool <lolzkiller1@gmail.com>
2018-10-23 17:10:45 +0200

    Added CreateComment to CommentQueries

--- Project_HNClone/Queries/CommentQueries.cs
+++ Project_HNClone/Queries/CommentQueries.cs
@@ -18,6 +18,41 @@ namespace Project_HNClone.Queries
             this.configuration = config;
         }
 
-        
+        public bool CreateComment(String content, int ownerID, int storyID)
+        {
+            bool verified = true;
+            using (SqlConnection connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+
+                try
+                {
+                    command.CommandText = "INSERT INTO Comments VALUES ('@CONTENT', '@OWNERID', '@STORYID');";
+                    command.Parameters.Add("@CONTENT", SqlDbType.NVarChar).Value = content;
+                    command.Parameters.Add("@OWNERID", SqlDbType.Int).Value = ownerID;
+                    command.Parameters.Add("@STORYID", SqlDbType.Int).Value = storyID;
+
+
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+
+                    // Check Error
+                    if (result < 0)
+                    {
+                        Console.WriteLine("Error inserting data into Database!");
+                        verified = false;
+                    }
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    // ignored
+                }
+            }
+            return verified;
+        }
     }
 }
