53d02a8fd0735ad4751f76fae676794b5fe242a1
Xinkai <stanley_h007@hotmail.com>
2018-11-02 00:36:28 +0100

    Nuget Packages update

--- Project_HNClone/Data/Comment.cs
+++ Project_HNClone/Data/Comment.cs
@@ -1,12 +1,14 @@
-﻿namespace Project_HNClone.Data
-{
-    public class Comment
-    {
-        public int id { get; set; }
-        public string content { get; set; }
-        public int ownerID { get; set; }
-        public int storyID { get; set; }
-        public string publishDate { get; set; }
+using System;
 
-    }
+namespace Project_HNClone.Data
+{
+  public class Comment
+  {
+    public int id { get; set; }
+    public string content { get; set; }
+    public int ownerID { get; set; }
+    public int storyID { get; set; }
+    public string publishDate { get; set; }
+
+  }
 }
--- Project_HNClone/Data/User.cs
+++ Project_HNClone/Data/User.cs
@@ -1,12 +1,14 @@
-﻿namespace Project_HNClone.Data
-{
-    public class User
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string password { get; set; }
-        public int karma { get; set; }
-        public string creationDate { get; set; }
+using System;
 
-    }
+namespace Project_HNClone.Data
+{
+  public class User
+  {
+    public int id { get; set; }
+    public string name { get; set; }
+    public string password { get; set; }
+    public int karma { get; set; }
+    public string creationDate { get; set; }
+
+  }
 }
