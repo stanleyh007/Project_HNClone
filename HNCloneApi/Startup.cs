2cf1245ad2c609038ba92e77368d6b4839575be5
Christian Philip Ege Nielsen <kagejohn@kagejohn.dk>
2018-11-11 20:45:55 +0100

    Added basic prometheus

--- HNCloneApi/Startup.cs
+++ HNCloneApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Prometheus;
 
 namespace HNCloneApi
 {
@@ -44,6 +45,7 @@ namespace HNCloneApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseMetricServer();
         }
     }
 }
